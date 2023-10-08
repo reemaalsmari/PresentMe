22abff4d100ee39f2898fca6f4c4769822793211
rsalsmari <alsmarireema@gmail.com>
2023-10-08 16:12:07 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by Reema Alsmari on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
