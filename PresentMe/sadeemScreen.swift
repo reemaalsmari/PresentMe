119c6a81e42a0b10e0fc1277ded9bff8d26c6780
SadeemAlthaali <sadeemalthaali@hotmail.com>
2023-10-09 16:12:26 +0300

    sadeem screen added

---
+++ PresentMe/sadeemScreen.swift
@@ -0,0 +1,18 @@
+//
+//  sadeemScreen.swift
+//  PresentMe
+//
+//  Created by Sadeem Althaali on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct sadeemScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sadeemScreen()
+}
