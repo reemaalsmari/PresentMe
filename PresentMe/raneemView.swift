7b18cd8eacbd2b1d9abc02ad5ce3290a938c8436
ran3m <ranemaldayl@gmail.com>
2023-10-09 16:33:11 +0300

    raneem screen added

---
+++ PresentMe/raneemView.swift
@@ -0,0 +1,18 @@
+//
+//  raneemView.swift
+//  PresentMe
+//
+//  Created by Ranem Aldayl on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct raneemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    raneemView()
+}
